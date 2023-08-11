8a0a01bff4d100a7c155abce2e37aca8d0df13d8
carolusquintus <carloschars@gmail.com>
2023-08-10 19:35:54 -0600

    Adding required endpoints

---
+++ src/main/java/dev/receipt/processor/infra/input/rest/ReceiptApi.java
@@ -0,0 +1,23 @@
+package dev.receipt.processor.infra.input.rest;
+
+import dev.receipt.processor.domain.entity.Points;
+import dev.receipt.processor.domain.entity.Receipt;
+import dev.receipt.processor.domain.entity.ReceiptId;
+import io.micronaut.http.annotation.*;
+
+import java.util.UUID;
+
+@Controller("/receipts")
+public class ReceiptApi {
+
+    @Post("/process")
+    public ReceiptId postReceiptProcess(@Body Receipt receipt) {
+        return new ReceiptId(UUID.randomUUID());
+    }
+
+    @Get("/{id}/points")
+    public Points getReceiptPoints(@PathVariable UUID id) {
+        return new Points(54);
+    }
+
+}
