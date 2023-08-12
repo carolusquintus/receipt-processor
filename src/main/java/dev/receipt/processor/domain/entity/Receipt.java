4f22cbdab6caa642f517da9c9bef8c2452450120
carolusquintus <carloschars@gmail.com>
2023-08-12 07:57:57 -0600

    Moving Receipt from immutable object to aggregate root object

--- src/main/java/dev/receipt/processor/domain/entity/Receipt.java
+++ src/main/java/dev/receipt/processor/domain/entity/Receipt.java
@@ -1,15 +1,22 @@
 package dev.receipt.processor.domain.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
-public record Receipt(
-    String retailer,
-    LocalDate purchaseDate,
-    LocalTime purchaseTime,
-    BigDecimal total,
-    List<Item> items
-) {
+@Data
+@Builder
+public class Receipt {
+
+    private ReceiptId id;
+    private String retailer;
+    private LocalDate purchaseDate;
+    private LocalTime purchaseTime;
+    private BigDecimal total;
+    private List<Item> items;
+
 }
