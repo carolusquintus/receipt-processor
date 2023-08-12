e9c879a864f82ac3d1aaf9fdc503bc52a2f02b64
carolusquintus <carloschars@gmail.com>
2023-08-12 12:20:58 -0600

    Adding ObjectMapper to parse Receipt to JSON

--- src/main/java/dev/receipt/processor/infra/output/redis/ReceiptRedisRepository.java
+++ src/main/java/dev/receipt/processor/infra/output/redis/ReceiptRedisRepository.java
@@ -1,16 +1,27 @@
 package dev.receipt.processor.infra.output.redis;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.receipt.processor.application.port.output.EntityRepository;
 import dev.receipt.processor.domain.entity.Receipt;
 import io.micronaut.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
 @Bean
+@RequiredArgsConstructor
 public class ReceiptRedisRepository implements EntityRepository<UUID, Receipt> {
 
+    private final ObjectMapper mapper;
     @Override
-    public Receipt save(Receipt type) {
+    public Receipt save(Receipt receipt) {
+        String receiptJson = null;
+        try {
+            receiptJson = mapper.writeValueAsString(receipt);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
