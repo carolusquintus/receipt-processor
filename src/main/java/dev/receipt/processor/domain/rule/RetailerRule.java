69e1394978c9abe7ab106b0be1acc0ea5b70dfea
carolusquintus <carloschars@gmail.com>
2023-08-13 22:57:10 -0600

    RetailerRule process implementation

--- src/main/java/dev/receipt/processor/domain/rule/RetailerRule.java
+++ src/main/java/dev/receipt/processor/domain/rule/RetailerRule.java
@@ -4,13 +4,19 @@ import dev.receipt.processor.domain.rule.shared.Rule;
 import io.micronaut.context.annotation.Bean;
 import jakarta.inject.Named;
 
+import java.util.regex.Pattern;
+
 @Bean
 @Named("retailerRule")
 public class RetailerRule implements Rule<String> {
 
     @Override
     public Long process(String retailer) {
-        return 0L;
+        return Pattern
+            .compile("[a-zA-Z0-9]")
+            .matcher(retailer)
+            .results()
+            .count();
     }
 
 }
