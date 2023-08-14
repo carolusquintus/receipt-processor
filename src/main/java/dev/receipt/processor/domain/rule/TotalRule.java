d5f75de9237b691c57ca7ae1fcea09fbe2d7edc5
carolusquintus <carloschars@gmail.com>
2023-08-14 12:56:01 -0600

    TotalRule process implementation

--- src/main/java/dev/receipt/processor/domain/rule/TotalRule.java
+++ src/main/java/dev/receipt/processor/domain/rule/TotalRule.java
@@ -6,13 +6,30 @@ import jakarta.inject.Named;
 
 import java.math.BigDecimal;
 
+import static java.math.BigDecimal.ONE;
+import static java.math.RoundingMode.HALF_UP;
+
 @Bean
 @Named("totalRule")
 public class TotalRule implements Rule<BigDecimal> {
 
     @Override
     public Long process(BigDecimal total) {
-        return 0L;
+        long sum = 0L;
+
+        total = total.setScale(2, HALF_UP);
+        var fraction = total.remainder(ONE);
+        var fractionAsInt = fraction.multiply(new BigDecimal(100)).intValue();
+
+        if (new BigDecimal("0.00").equals(fraction)) {
+            sum += 50L;
+        }
+
+        if (fractionAsInt % 25 == 0) {
+            sum += 25L;
+        }
+
+        return sum;
     }
 
 }
