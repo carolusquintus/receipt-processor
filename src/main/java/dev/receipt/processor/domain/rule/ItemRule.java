ca2624097663f78fa9234bceb3f70bbe9a59c286
carolusquintus <carloschars@gmail.com>
2023-08-14 13:24:34 -0600

    ItemRule process implementation

--- src/main/java/dev/receipt/processor/domain/rule/ItemRule.java
+++ src/main/java/dev/receipt/processor/domain/rule/ItemRule.java
@@ -5,12 +5,24 @@ import dev.receipt.processor.domain.rule.shared.Rule;
 import io.micronaut.context.annotation.Bean;
 import jakarta.inject.Named;
 
+import java.math.BigDecimal;
+
+import static java.math.RoundingMode.UP;
+
 @Bean
 @Named("itemRule")
 public class ItemRule implements Rule<Item> {
 
     @Override
     public Long process(Item item) {
+        var descriptionLength = item.shortDescription().trim().length();
+
+        if (descriptionLength % 3 == 0) {
+            return item
+                .price().multiply(new BigDecimal("0.2"))
+                .setScale(0, UP).longValue();
+        }
+
         return 0L;
     }
 
