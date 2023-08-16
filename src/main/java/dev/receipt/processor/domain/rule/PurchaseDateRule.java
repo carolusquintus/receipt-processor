a71da8b17f8f43c79e367c75e4daa38b3ebe2c13
carolusquintus <carloschars@gmail.com>
2023-08-16 07:25:39 -0600

    PurchaseDateRuleSpec test & adjustments

--- src/main/java/dev/receipt/processor/domain/rule/PurchaseDateRule.java
+++ src/main/java/dev/receipt/processor/domain/rule/PurchaseDateRule.java
@@ -5,6 +5,9 @@ import io.micronaut.context.annotation.Bean;
 import jakarta.inject.Named;
 
 import java.time.LocalDate;
+import java.util.Objects;
+
+import static java.util.Objects.isNull;
 
 @Bean
 @Named("purchaseDateRule")
@@ -12,6 +15,9 @@ public class PurchaseDateRule implements Rule<LocalDate> {
 
     @Override
     public Long process(LocalDate purchaseDate) {
+        if (isNull(purchaseDate)) {
+            return 0L;
+        }
         var day = purchaseDate.getDayOfMonth();
         return (day % 2 == 0) ? 0L : 6L;
     }
---
+++ src/test/groovy/dev/receipt/processor/domain/rule/PurchaseDateRuleSpec.groovy
@@ -0,0 +1,28 @@
+package dev.receipt.processor.domain.rule
+
+import spock.lang.Specification
+
+import java.time.LocalDate
+
+class PurchaseDateRuleSpec extends Specification {
+
+     def "Calculates points if day of month is odd when purchaseDate: #purchaseDate == #points points" () {
+        given:
+        def rule = new PurchaseDateRule()
+
+        when:
+        def result = rule.process(purchaseDate)
+
+        then:
+        result == points
+
+        where:
+        purchaseDate              || points
+        null                      || 0
+        LocalDate.of(2022, 1, 1)  || 6
+        LocalDate.of(2022, 1, 2)  || 0
+        LocalDate.of(2022, 3, 20) || 0
+
+    }
+
+}
